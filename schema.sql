a1087ee90d5ba0ddcc9721b71d52b30f4884a0e6
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-02-09 10:32:39 -0700

    created schemas db

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS tracker_db;
+
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department (
+  id int AUTO_INCREMENT NOT NULL,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id int AUTO_INCREMENT NOT NULL,
+  title varchar(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id int NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+  id int AUTO_INCREMENT NOT NULL,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id int NOT NULL,
+  manager_id int NOT NULL,
+  PRIMARY KEY(id)
+);
